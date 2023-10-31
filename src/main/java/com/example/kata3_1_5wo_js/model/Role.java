9a22792f3778db5d3622a18d546cd7e808aff428
Myaksih <nnkozhemyakin@yandex.ru>
2023-10-31 17:40:36 +0300

    commit2

--- src/main/java/com/example/kata3_1_5wo_js/model/Role.java
+++ src/main/java/com/example/kata3_1_5wo_js/model/Role.java
@@ -8,7 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -24,6 +26,9 @@ public class Role implements GrantedAuthority {
 
     @Column(name = "name")
     private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
 
     public Role(String name) {
         this.name = name;
